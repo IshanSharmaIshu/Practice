4eacbbdf3f6a163bc9433b90ff7755e4c2ba7d5b
Ishan Sharma <137897672+IshanSharmaIshu@users.noreply.github.com>
2025-03-19 15:58:59 +0530

    Update terraform ec2 creation.tf

--- terraform ec2 creation.tf
+++ terraform ec2 creation.tf
@@ -1,68 +1,26 @@
-# 1. Install Terraform from the Terraform.io site 
-# 2. sudo apt install python3-pip
-# 3. For Credentials save: mkdir -p ~/.aws
-# 4. nano ~/.aws/credentials
-# 5. mkdir terraform
-# 6. cd terraform/
-# 7. main.tf
-
-
-
-
 #Cloud Provider and Region on which Creating the Instance
 provider "aws" {
-    region = "region.name.type.here"
+    region = "us-east-1"
 }
 
 #Describe the Instance Type & Instance Image(AMI)
 resource "aws_instance" "my_ec2_instance" {
-    ami = "ami.id.in(x86).paste.here"
+    ami = "ami-0e1bed4f06a3b463d"
     instance_type = "t2.micro"
-
-    #Paste Your Creted Security Group ID
-    vpc_security_group_ids = ["paste.sg.id.after.creting.the.sg.by.tf.code"]
 
     #Attach the Storage Volume Size & it's Volume type
     root_block_device {
         volume_type = "gp2"
-        volume_size = type.storage.which.you.want
+        volume_size = 10
     }
 
     #Instance Name
     tags = {
-        Name = "type.instance.name.here"
+        Name = "code-pipeline"
     }
 
     #Key Pair Name
-    key_name = "type.key-pair.name.here"
-}
-
-#Security Group Name
-resource "aws_security_group" "web_sg" {
-    name = "type.sg.name.here.which.you.want.to.create"
-    description = "Allow HTTP and HTTPS traffic"
-
-    #Attach SSH Port
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    #Attach HTTP Port
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    #Attach HTTPS Port
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    key_name = "n.virginia-11"
 }
 
 #These Instance ID and Public Ip Assign after the Instance Created
